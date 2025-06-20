c152ec3eebfb5836d2505e795fbb72e3f477a788
Miguel Bañuelos <482200146@alumnos.utzac.edu.mx>
2025-06-20 15:53:56 -0600

    Login Iniciado

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor( private http:HttpClient) { }
+  url = environment.apiUrl
+
+      register(
+      username:string,
+      email:string,
+      password:string){
+      return this.http.post(this.url + "/auth/local/register",{username,email,password});
+    }
+
+    login(data:any){
+      return this.http.post(this.url + "/auth/local",data);
+    }
 }
