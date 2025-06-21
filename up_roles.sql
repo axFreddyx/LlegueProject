d5f92d597cc9b2bdbd3523fcde95ca3b4ce1a4a8
Alfredo <alfredosolis736@outlook.com>
2025-06-20 23:00:20 -0600

    Definición de roles

--- up_roles.sql
+++ up_roles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 20-06-2025 a las 00:03:32
+-- Tiempo de generación: 21-06-2025 a las 04:58:56
 -- Versión del servidor: 8.0.34
 -- Versión de PHP: 8.3.8
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `up_roles`
 --
 
-CREATE TABLE `up_roles` (
+CREATE TABLE IF NOT EXISTS `up_roles` (
   `id` int UNSIGNED NOT NULL,
   `document_id` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `up_roles` (
   `created_by_id` int UNSIGNED DEFAULT NULL,
   `updated_by_id` int UNSIGNED DEFAULT NULL,
   `locale` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
 
 --
 -- Volcado de datos para la tabla `up_roles`
@@ -50,7 +50,7 @@ INSERT INTO `up_roles` (`id`, `document_id`, `name`, `description`, `type`, `cre
 (2, 'iyr2s2xnvcwpquh3d175yhgk', 'Public', 'Default role given to unauthenticated user.', 'public', '2025-06-13 16:29:56.304000', '2025-06-13 16:29:56.304000', '2025-06-13 16:29:56.304000', NULL, NULL, NULL),
 (3, 'dne5e6uy5luun25yr1rd6n3s', 'Persona Autorizada', 'Rol para personas autorizadas de los alumnos', 'persona_autorizada', '2025-06-18 18:07:59.767000', '2025-06-19 17:45:38.517000', '2025-06-18 18:07:59.771000', NULL, NULL, NULL),
 (4, 'otapmjgpyq7akmy7wd9gniq8', 'Docente', 'Rol de los docentes de alumnos', 'docente', '2025-06-18 18:08:44.286000', '2025-06-19 17:46:23.538000', '2025-06-18 18:08:44.286000', NULL, NULL, NULL),
-(5, 'k2oqjvfp3nwm4alxeufeph2x', 'Admin', 'Rol de admin', 'admin', '2025-06-19 17:43:43.864000', '2025-06-19 17:43:43.864000', '2025-06-19 17:43:43.866000', NULL, NULL, NULL);
+(5, 'k2oqjvfp3nwm4alxeufeph2x', 'Admin', 'Rol de admin', 'admin', '2025-06-19 17:43:43.864000', '2025-06-20 22:26:06.164000', '2025-06-19 17:43:43.866000', NULL, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
