5010144958d7429ff20143caea800ec9977baacf
Alfredo <alfredosolis736@outlook.com>
2025-06-21 14:41:24 -0600

    Cambios de controlador user

--- backend/src/api/alumno/routes/alumno.ts
+++ backend/src/api/alumno/routes/alumno.ts
@@ -4,7 +4,21 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::alumno.alumno');
+// export default factories.createCoreRouter('api::alumno.alumno');
+
+export default {
+  routes: [
+    {
+      method: 'GET',
+      path: '/alumno',
+      handler: 'alumno.find',
+      config: {
+        policies: [],
+      },
+    },
+  ],
+};
+
 
 // export default {
 //     method : "GET",
