71c0bee9eee9c5d1087be52bf39e7f8658d05565
Alfredo <alfredosolis736@outlook.com>
2025-06-21 19:21:47 -0600

    Cambio de codigo en rutas para alumno

--- backend/src/api/alumno/routes/alumno.ts
+++ backend/src/api/alumno/routes/alumno.ts
@@ -4,20 +4,20 @@
 
 import { factories } from '@strapi/strapi';
 
-// export default factories.createCoreRouter('api::alumno.alumno');
+export default factories.createCoreRouter('api::alumno.alumno');
 
-export default {
-  routes: [
-    {
-      method: 'GET',
-      path: '/alumno',
-      handler: 'alumno.find',
-      config: {
-        policies: [],
-      },
-    },
-  ],
-};
+// export default {
+//   routes: [
+//     {
+//       method: 'GET',
+//       path: '/alumno',
+//       handler: 'alumno.find',
+//       config: {
+//         policies: [],
+//       },
+//     },
+//   ],
+// };
 
 
 // export default {
