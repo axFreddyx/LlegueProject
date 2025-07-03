f4b48b6f6907b89c62d82ad23fe47cbb0010937e
Miguel Bañuelos <482200146@alumnos.utzac.edu.mx>
2025-07-03 15:52:32 -0600

    Correción Estilo GyG

--- frontend/src/app/admin/gradoygrupo/gradoygrupo.page.ts
+++ frontend/src/app/admin/gradoygrupo/gradoygrupo.page.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OverlayEventDetail } from '@ionic/core/components';
 @Component({
   selector: 'app-gradoygrupo',
   templateUrl: './gradoygrupo.page.html',
   styleUrls: ['./gradoygrupo.page.scss'],
-  standalone:false
+  standalone: false
 })
 export class GradoygrupoPage implements OnInit {
 
   constructor(
-    
+
   ) { }
+
+  isModalOpen = false;
+
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+    this.isModalOpen = false;
+  }
 
   ngOnInit() {
   }
   gradosEjemplo = [
-  { nombre: '1°', grupos: ['A', 'B'] },
-  { nombre: '2°', grupos: ['A'] },
-  { nombre: '3°', grupos: ['A', 'B', 'C'] },
-  { nombre: '4°', grupos: ['A', 'B', ] },
-  { nombre: '5°', grupos: ['A', 'B', ] },
-  { nombre: '6°', grupos: ['A',] },
-];
+    { nombre: '1°', grupos: ['A', 'B'] },
+    { nombre: '2°', grupos: ['A'] },
+    { nombre: '3°', grupos: ['A', 'B', 'C'] },
+    { nombre: '4°', grupos: ['A', 'B',] },
+    { nombre: '5°', grupos: ['A', 'B',] },
+    { nombre: '6°', grupos: ['A',] },
+  ];
 
 
 }
