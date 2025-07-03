cebb8d05317c8ee3b1101be2515475c120aeffd6
Miguel Bañuelos <482200146@alumnos.utzac.edu.mx>
2025-07-02 18:55:31 -0600

    Seleccionar Hijos

--- frontend/src/app/admin/pautorizada/ver/ver.page.ts
+++ frontend/src/app/admin/pautorizada/ver/ver.page.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-ver',
   templateUrl: './ver.page.html',
   styleUrls: ['./ver.page.scss'],
-  standalone:false
+  standalone: false
 })
 export class VerPage implements OnInit {
 
   constructor() { }
+
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+    this.isModalOpen = false;
+  }
+
 
   ngOnInit() {
   }
