059c12faeabed7ae97cd3e1017bd561fc632a85d
Miguel Bañuelos <482200146@alumnos.utzac.edu.mx>
2025-08-15 15:03:06 -0600

    Cambios

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.20.10.3:1337/api',
-  //apiUrl: 'http://localhost:1337/api',
+  // apiUrl: 'http://172.20.10.3:1337/api',
+  apiUrl: 'http://localhost:1337/api',
 
 };
