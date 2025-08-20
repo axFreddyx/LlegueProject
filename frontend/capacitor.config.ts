b2835938411d7f3d4a5c3edf4a4c4e318133ae22
Miguel Bañuelos <482200146@alumnos.utzac.edu.mx>
2025-08-20 17:35:15 -0600

    Comienzo para hacer Notificaciones

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,11 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-<<<<<<< HEAD
-=======
-
->>>>>>> bb255f3d7e9eb35790fa6329049e45aa43c4a70d
-  appId: 'com.llegueFJP',
+  appId: 'com.llegue3jotas',
   appName: 'frontend',
   webDir: 'www',
 };
