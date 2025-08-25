f612fe76fbf7075bb08e75e35e1c7f0366734630
Pepe Toño Hrz <joseach912@gmail.com>
2025-08-25 15:45:45 -0600

    Mostrar foto en perfil

--- frontend/src/app/perfil/perfil.page.ts
+++ frontend/src/app/perfil/perfil.page.ts
@@ -1,39 +1,50 @@
+// perfil.page.ts
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Storage } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
-  standalone:false
+  standalone: false
 })
 export class PerfilPage implements OnInit {
+  token = '';
+  me: any = {};
 
   constructor(
     private api: ApiService,
     private storage: Storage
-  ) { }
-
-  token = "";
-  me:any = {};
+  ) {}
 
   async ngOnInit() {
-    this.token = await this.storage.get("token")
-    this.getMe();
+    this.token = await this.storage.get('token');
+    await this.getMe();
   }
 
-  async getMe(){
-    await this.api.getUserByMe().then((res:any) => {
-      console.log(res.data);
+  async getMe() {
+    try {
+      const res: any = await this.api.getUserByMe();
       this.me = res.data;
-    }).catch((err:any) => {
-      console.error(err)
-    })
+      console.log('ME =>', this.me);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-  async getFoto(){
-    // await this.api.
-  }
+  get fotoUrl(): string {
+    const media = this.me?.foto;
+    const urlRelativa =
+      Array.isArray(media) ? media?.[0]?.url : media?.url;
 
+    if (!urlRelativa) {
+      return 'assets/img/alumno_defecto.jpg'; 
+    }
+
+    // environment.apiUrl = 'http://localhost:1337/api' -> necesitamos el host sin '/api'
+    const host = environment.apiUrl.replace(/\/api\/?$/, '');
+    return `${host}${urlRelativa}`;
+  }
 }
