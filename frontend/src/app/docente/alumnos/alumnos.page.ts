df1b0512259061f27144170a50d6c96f58728401
Miguel Bañuelos <482200146@alumnos.utzac.edu.mx>
2025-08-26 14:56:02 -0600

    Quitar

--- frontend/src/app/docente/alumnos/alumnos.page.ts
+++ frontend/src/app/docente/alumnos/alumnos.page.ts
@@ -56,7 +56,6 @@ export class AlumnosPage implements OnInit {
     this.detectDevice();
     await this.getMe();
     await this.getLlegada();
-    this.notification();
     setInterval(() => {
       this.getLlegada();
     }, 5000); // Cada 1 segundo
@@ -174,50 +173,6 @@ export class AlumnosPage implements OnInit {
         this.router.navigateByUrl('/login');
       }, 1500);
     });
-  }
-
-  notification() {
-    console.log('Initializing Ver por cuales alumnos han llegado');
-
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    PushNotifications.requestPermissions().then(result => {
-      if (result.receive === 'granted') {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
-
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration',
-      (token: Token) => {
-        alert('Push registration success, token: ' + token.value);
-      }
-    );
-
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        alert('Error on registration: ' + JSON.stringify(error));
-      }
-    );
-
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification: PushNotificationSchema) => {
-        alert('Push received: ' + JSON.stringify(notification));
-      }
-    );
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (notification: ActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
-      }
-    );
   }
 
   // 🖼️ Helper para obtener la URL de la foto desde Strapi (v4/v5)
